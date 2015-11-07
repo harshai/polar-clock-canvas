c8aa77b731c5b99a8b0d03210035d84121dd50f0
harshai <harshai@thoughtworks.com>
2015-11-07 21:29:23 +0530

    Adding black disc

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 ~function(document)   {
   window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
                                 window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-
-  var createCanvas = function() {
+  var w, h, full, quarter,
+  createCanvas = function() {
     var canvas = document.getElementById('polar-clock') ?
                  document.getElementById('polar-clock') :
                  document.createElement('canvas'),
     cxt = canvas.getContext('2d');
     if(cxt) {
-      cxt.canvas.width  = window.innerWidth;
-      cxt.canvas.height = window.innerHeight;
+      cxt.canvas.width = w = window.innerWidth;
+      cxt.canvas.height = h = window.innerHeight;
       canvas.setAttribute('id', 'polar-clock');
       document.body.appendChild(canvas);
     }
@@ -17,26 +17,27 @@
   },
 
   getClockDimensions = function(scale){
-    return Math.min(1024/2, (Math.min(window.innerWidth, window.innerHeight)/2)) * scale;
+    return Math.min(1024/2, (Math.min(w, h)/2)) * scale;
   },
 
   createCircle = function(cxt, radius, perc) {
-    var x = window.innerWidth/2,
-        y = window.innerHeight/2,
+    var x = w/2,
+        y = h/2,
         counterClockwise =  false,
+        color = generateColor(perc);
         full = Math.PI * 2,
         quarter = Math.PI / 2,
-        color = generateColor(perc)
 
     cxt.shadowOffsetX = 0;
     cxt.shadowOffsetY = 0;
-    cxt.shadowBlur = 5;
+    cxt.shadowBlur = 4;
     cxt.lineWidth = 0.08 * getClockDimensions(0.8);
     cxt.beginPath();
     cxt.shadowColor = color;
     cxt.strokeStyle = color;
     cxt.arc(x, y, radius, -(quarter), ((full) * perc) - quarter, counterClockwise);
     cxt.stroke();
+    cxt.closePath();
   },
 
   createParams = function() {
@@ -66,24 +67,27 @@
     return "hsl(" + (360 * (value)) + ", 40%, 50% )";
   },
 
-  animate = function() {
-    var cxt = createCanvas(),
-        radius;
-    if (cxt) {
-      cxt.clearRect(0, 0, cxt.canvas.width, cxt.canvas.height);
-      createParams().forEach(function(el, i, arr) {
-        radius = getClockDimensions(0.8) * el.factor - 20;
-        createCircle(cxt, radius, el.value);
-      });
+  animate = function(cxt) {
+    var radius;
+    cxt.clearRect(0, 0, cxt.canvas.width, cxt.canvas.height);
+    cxt.arc(w/2, h/2, getClockDimensions(0.65), -quarter, full - quarter);
+    cxt.fillStyle = "#333";
+    cxt.fill();
+    createParams().forEach(function(el, i, arr) {
+      radius = getClockDimensions(0.8) * el.factor - 20;
+      createCircle(cxt, radius, el.value);
+    });
+    requestAnimationFrame(function(){
+      animate(cxt);
+    });
+  };
 
-      requestAnimationFrame(function(){
-        animate(cxt);
-      });
+  window.onload = function() {
+    if(cxt = createCanvas()){
+      animate(cxt);
     } else {
       document.write("This is an experiment that depends on canvas, which is not supported by your browser. Please try using a modern browser.")
     }
-  };
-
-  window.onload = animate();
+  }
 
 }(document);
